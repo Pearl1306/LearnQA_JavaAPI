09f29796054b33aaea18db5f544ba0a2e0ccca04
Pearl <linatali1306@gmail.com>
2022-09-15 03:48:23 -0400

    Homework

---
+++ src/test/java/HomeworkHello.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class HomeworkHello {
+
+    @Test
+
+    public void testHelloFromNatali(){
+        System.out.println("Hello from Natali");
+    }
+}
